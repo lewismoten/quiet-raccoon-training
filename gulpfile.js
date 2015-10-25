bbc0b78626e95c24a72929cbee3a84bd558fbff9
Lewie <lewismoten@gmail.com>
2015-10-25 02:34:43 -0400

    Setup minificationtask

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 (function gulpFile(require, console){
 
-	var gulp = require('gulp');
+	var gulp = require('gulp'),
+		minify = require('gulp-minify');
 
 	gulp.task('default', function() {
 
 		console.log('This is the default task...');
+
+	});
+
+	gulp.task('compress', function() {
+
+		var options = {
+			exclude: ['tasks'],
+			ignoreFiles: ['.combo.js', '-min.js']
+		};
+
+		gulp.src('lib/*.js')
+
+			.pipe(minify(options))
+
+			.pipe(gulp.dest('dist'));
 
 	});
 
