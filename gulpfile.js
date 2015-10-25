97c710553b9c91ad43a629fb33771f6732168147
Lewie <lewismoten@gmail.com>
2015-10-25 01:57:07 -0400

    Setup task/build runner (Gulp)

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+(function gulpFile(require, console){
+
+	var gulp = require('gulp');
+
+	gulp.task('default', function() {
+
+		console.log('This is the default task...');
+
+	});
+
+})(require, console);
